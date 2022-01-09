d3f7c921bd5f2844db3935294603832b1196fd7c
daniel natale <dnatale667@gmail.com>
2022-01-09 12:06:52 -0500

    fjhfjfjd

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+
 const PORT = process.env.PORT || 8080;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker"
 
@@ -16,11 +17,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
+mongoose.connection.on('connected', () => {
+  console.log("u in")
+})
 // routes
 app.use(require("./routes/api.js"));
 
